0f8112dc16429624c9fe0e1f01f411ea24e74a53
Joonhyeok Im <itanoss@gmail.com>
2016-11-18 16:11:23 +0900

    Implement printing syslog continuously
    Using EventSource in web side, it provides the content which is from tail -f /var/log/syslog.
    But it is too slow, which interval seems to be almost 1-2 minutes, which should be enhanced.

--- app.py
+++ app.py
@@ -1,9 +1,115 @@
-from flask import Flask
+# ref: http://flask.pocoo.org/snippets/116/
+# ref: http://stackoverflow.com/questions/12523044/how-can-i-tail-a-log-file-in-python
+
+import gevent
+from gevent.wsgi import WSGIServer
+from gevent.queue import Queue
+
+from flask import Flask, Response
+
+import time
+import threading, subprocess
+
 app = Flask(__name__)
- 
+subscriptions = []
+
+def tail(filename):
+    p = subprocess.Popen(['tail','-F',filename],\
+            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    while True:
+        line = p.stdout.readline()
+        print('Tail thread({}): {}'.format(time.time(), line))
+        for sub in subscriptions[:]:
+            sub.put(line)
+        time.sleep(2)
+
+# gevent.spawn(tail)
+t = threading.Thread(target=tail, args=('/var/log/syslog',))
+t.daemon = True
+t.start()
+
+# SSE "protocol" is described here: http://mzl.la/UPFyxY
+class ServerSentEvent(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.event = None
+        self.id = None
+        self.desc_map = {
+            self.data : "data",
+            self.event : "event",
+            self.id : "id"
+        }
+
+    def encode(self):
+        if not self.data:
+            return ""
+        lines = ["%s: %s" % (v, k) 
+                 for k, v in self.desc_map.iteritems() if k]
+        
+        return "%s\n\n" % "\n".join(lines)
+
+
+# Client code consumes like this.
 @app.route("/")
-def hello():
-    return "Welcome to Python Flask!"
- 
+def index():
+    debug_template = """
+     <html>
+       <head>
+       </head>
+       <body>
+         <h1>Server sent events</h1>
+         <div id="event"></div>
+         <script type="text/javascript">
+
+         var eventOutputContainer = document.getElementById("event");
+         var evtSrc = new EventSource("/subscribe");
+
+         evtSrc.onmessage = function(e) {
+             console.log(e.data);
+             var p = document.createElement('p');
+             p.innerHTML = e.data;
+             eventOutputContainer.appendChild(p);
+         };
+
+         </script>
+       </body>
+     </html>
+    """
+    return(debug_template)
+
+@app.route("/debug")
+def debug():
+    return "Currently %d subscriptions" % len(subscriptions)
+
+@app.route("/publish")
+def publish():
+    #Dummy data - pick up from request for real data
+    def notify():
+        msg = str(time.time())
+        for sub in subscriptions[:]:
+            sub.put(msg)
+    
+    gevent.spawn(notify)
+    
+    return "OK"
+
+@app.route("/subscribe")
+def subscribe():
+    def gen():
+        q = Queue()
+        subscriptions.append(q)
+        try:
+            while True:
+                result = q.get()
+                ev = ServerSentEvent(str(result))
+                yield ev.encode()
+        except GeneratorExit: # Or maybe use flask signals
+            subscriptions.remove(q)
+
+    return Response(gen(), mimetype="text/event-stream")
+
 if __name__ == "__main__":
-    app.run()
+    app.debug = True
+    server = WSGIServer(("", 5000), app)
+    server.serve_forever()
